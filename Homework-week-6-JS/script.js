e4940bf7c663df903fc55a25461929bf527522be
Gabriel03 <gabi.rezeanu20@gmail.com>
2022-06-07 21:13:04 +0300

    adding  homework

---
+++ Homework-week-6-JS/script.js
@@ -0,0 +1,68 @@
+// Question 1
+// 1.local
+// 2.yes but only inside that function or any nested functions
+// 3.the console will display greeting
+
+// Question 2
+// 1.global
+// 2.yes
+// 3.error
+
+// Question 3
+// 1.local
+// 2.out of scope
+// 3.no
+// 4.the console will display the string "snap pea"
+// 5.error
+
+// Question 4
+// 1.global
+// 2.global
+// 3.the console will display the string "Superman"
+// 4.the consle will display the string "Batman"
+// 5.no
+
+// Question 5
+// 1.global
+// 2.the console will display the string "beep"
+// 3.expression
+// 4.we delete the word function and the console will display Syntax Error
+// 5.the console will still display the string "beep"
+// 6.the console will show "beep" because the variable is in the global scope
+// 7.Error : beep is not defined
+
+// Question 6
+var sumOfNums = function (numsArray) {
+
+};
+
+// Question 7
+
+var numsGreaterThanTen = function (numsArray) {
+  // Your Code Here
+};
+
+// Question 8
+
+var noneHaveTwoOrMoreVowels = function (words) {
+  // Your Code Here
+};
+
+// Question 9
+var hasAtLeastNVowels = function (word, n) {
+  // Your Code Here
+};
+
+// Question 10
+var buildObjectFromWords = function (words) {
+  // Your Code Here
+};
+
+/// DO NOT EDIT BELOW THIS LINE ///
+module.exports = {
+  sumOfNums: sumOfNums,
+  numsGreaterThanTen: numsGreaterThanTen,
+  noneHaveTwoOrMoreVowels: noneHaveTwoOrMoreVowels,
+  buildObjectFromWords: buildObjectFromWords,
+  allStartingWithA: allStartingWithA,
+};
