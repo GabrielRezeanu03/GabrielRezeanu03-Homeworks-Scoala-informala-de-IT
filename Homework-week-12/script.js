6d6e1757833acdb2979c02741d95fc8610f9653c
Gabriel03 <gabi.rezeanu20@gmail.com>
2022-06-08 21:30:21 +0300

    adding homework

---
+++ Homework-week-12/script.js
@@ -0,0 +1,120 @@
+"use strict";
+
+window.addEventListener("hashchange", () => {
+  console.log("hash was change");
+});
+const backBtn = document.querySelector(".back");
+const forwardBtn = document.querySelector(".forward");
+const gotoBtn = document.querySelector(".goto");
+
+backBtn.addEventListener("click", (e) => {
+  history.back();
+});
+forwardBtn.addEventListener("click", (e) => {
+  history.forward();
+});
+
+gotoBtn.addEventListener("click", () => {
+  history.pushState({}, "", "#contact");
+});
+
+window.addEventListener("popstate", (e) => {
+  console.log(
+    "location: " + document.location + ", state: " + JSON.stringify(e.state)
+  );
+});
+
+// ES6 classes
+
+class Vehicle {
+  constructor(name, color, engine, fuel, wheels) {
+    this.name = name;
+    this.color = color;
+    this.engine = engine;
+    this.fuel = fuel;
+    this.wheels = wheels;
+  }
+
+  drive() {
+    console.log(`${this.name} can drive`);
+  }
+
+  park() {
+    console.log(`${this.name} can drive`);
+  }
+}
+
+class Car extends Vehicle {
+  constructor(name, color, engine, fuel) {
+    super(name, color, engine, fuel);
+    this.wheels = 4;
+  }
+}
+
+class Moto extends Vehicle {
+  constructor(name, color, engine, fuel) {
+    super(name, color, engine, fuel);
+    this.wheels = 2;
+  }
+}
+
+class Bus extends Vehicle {
+  constructor(name, color, engine, fuel) {
+    super(name, color, engine, fuel);
+    this.wheels = 16;
+    this.color = "Yellow";
+  }
+}
+
+class Engine {
+  constructor(name, capacity) {
+    this.name = name;
+    this.capacity = capacity;
+  }
+  stop() {
+    console.log("Has stopped");
+  }
+  start() {
+    console.log("starting");
+  }
+  run() {
+    console.log("running");
+  }
+}
+
+class Fleet {
+  _fleet = [];
+  constructor(lotNumber, vehicle) {
+    this.vehicle = vehicle;
+    this.lotNumber = lotNumber;
+  }
+
+  make() {
+    for (let i = 0; i < this.lotNumber; i++) {
+      this._fleet.push(this.vehicle);
+    }
+  }
+  getFleet() {
+    return this._fleet;
+  }
+}
+
+const bus = new Bus("volvo", "", "4.0", "diesel");
+const mercedes = new Car("Mercedes", "black", "2.0", "diesel");
+const engine = new Engine("v2", "2.0");
+
+class Highway {
+  constructor(...vehicles) {
+    this.vehicles = vehicles;
+  }
+
+  isDriving() {
+    for (let vehicle of this.vehicles) {
+      if (Vehicle.prototype.isPrototypeOf(vehicle)) {
+        vehicle.drive();
+      }
+    }
+  }
+}
+
+const highway = new Highway(bus, mercedes, engine);
